dbfb6f7f1da15512ce89aa5653b63df670827906
Willian Serafim <willian_50@ymail.com>
2020-11-07 21:01:31 -0300

    baseURL correction

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://marvel-3e933.firebaseio.com';
-axios.defaults.baseURL = 'https://marvel-fc282.firebaseio.com';
+axios.defaults.baseURL = 'INSIRA AQUI SUA URL DO FIREBASE';
 
 Vue.use({
   install() {
